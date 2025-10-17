b64db775392464cf644f1ead19a9f788505d7bbd
RafaelRFAndrade <110788109+RafaelRFAndrade@users.noreply.github.com>
2025-10-16 21:32:58 -0300

    fix: sonar não pega chave diretamente

--- Application/Services/AutenticacaoService.cs
+++ Application/Services/AutenticacaoService.cs
@@ -21,7 +21,6 @@ namespace Application.Services
         public string GenerateToken(Usuario usuario)
         {
             var tokenHandler = new JwtSecurityTokenHandler();
-            var key = Encoding.ASCII.GetBytes(_configuration["Jwt:Key"]);
 
             var tokenDescriptor = new SecurityTokenDescriptor
             {
@@ -36,7 +35,7 @@ namespace Application.Services
                 Expires = DateTime.UtcNow.AddMinutes(double.Parse(_configuration["Jwt:ExpireMinutes"])),
                 Issuer = _configuration["Jwt:Issuer"],
                 Audience = _configuration["Jwt:Audience"],
-                SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature)
+                SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(Encoding.ASCII.GetBytes(_configuration["Jwt:Key"])), SecurityAlgorithms.HmacSha256Signature)
             };
 
             var token = tokenHandler.CreateToken(tokenDescriptor);
